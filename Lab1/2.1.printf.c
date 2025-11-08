6565f6e19f6832d80a57857652eb41454b380e2e
Lê Thị Hồng Đào <148214402+daolth6@users.noreply.github.com>
2025-11-08 14:22:17 +0700

    Nop lab 1

---
+++ Lab1/2.1.printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+int main(){
+	printf("______________________________________\n");
+	printf("|WELCOME TO BAYTT'S ASSIGNMENT COM108|\n");
+	printf("-----------------MENU-----------------\n");
+	printf("1. Chuc nang tim tinh nguyen cua mot so\n");
+	printf("2. Chuc nang tim UCLN va BCNN\n");
+	printf("3. Chuc nang tinh tien Karaoke\n");
+	printf("4. Coming soon...\n");
+	printf("6. Coming soon...\n");
+	printf("8. Coming soon...\n");
+	printf("10.Coming soon...\n");
+	printf("0. Exit\n");
+	printf("\nMoi ban nhap lua chon chuong trinh: "); 
+	return 0;
+}
